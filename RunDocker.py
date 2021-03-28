be02ca7a43b4a606d3486ff3388b0a95c868ca96
bhat sumair <35607482+Bhatsumair@users.noreply.github.com>
2021-03-29 01:43:37 +0530

    launching docker

---
+++ RunDocker.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+print("content-type: text/html")
+print()
+
+import subprocess
+import cgi
+
+form = cgi.FieldStorage()
+
+#osname = input("Enter ur os name : ")
+osname = form.getvalue(" x")
+
+cmd = "docker run -dit --name {0} centos:7".format(osname)
+
+output = subprocess.getstatusoutput(cmd)
+
+status = output[0]
+out = output[1]
+
+if status == 0:
+   print("os launched name {} ..".format(osname))
+else:
+   print("some eror : {}".format(out))
